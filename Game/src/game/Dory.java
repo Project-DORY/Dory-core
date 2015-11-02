4f3dfad4bfdfea47d73d45eacafd1a16eacdedff
ChoiJIN <zuharu@gmail.com>
2015-11-02 19:48:58 +0900

    #1 empty class set

--- Game/src/game/Dory.java
+++ Game/src/game/Dory.java
@@ -1,8 +1,14 @@
 package game;
 
-public class Dory {
+import view.IView;
+import view.TextView;
 
+public class Dory {
 	public static void main(String[] args) {
+		IView view;
+		view = new TextView();
+		
+		
 		System.out.println("merong");
 	}
 
