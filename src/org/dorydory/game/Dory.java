bde5ec770c723058ce013908e98b1e41d5cb2034
xyhatte <dglim27@gmail.com>
2015-11-25 20:01:54 +0900

    making new one-time running game

--- src/org/dorydory/game/Dory.java
+++ src/org/dorydory/game/Dory.java
@@ -7,14 +7,16 @@ import org.dorydory.view.IView;
 import org.dorydory.view.TextView;
 
 public class Dory {
+	private Npc helper = new Npc();
+	helper.setName(wendy);
+	
 	public static void main(String[] args) {
 		IView view;
 		view = new TextView();
 		
-		ArrayList<Unit> battleList = null;
-		
-		
-		System.out.println("merong");
+		System.out.println("Welcome to  the DORY Game!");
+		System.out.println("DORY 게임에 오신것을 환영합니다! 저는 여러분의 도우미 ");
+		System.out.format("%s", helper.name());
 	}
 
 }
---
+++ src/org/dorydory/game/Npc.java
@@ -0,0 +1,9 @@
+package org.dorydory.game;
+
+public class Npc {
+	private String name = null;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
